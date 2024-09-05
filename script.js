6077bcb0414f42ca8abdf5d69547eae1210b2468
Abhishek Gothankar <abhishekgothankar01@gmail.com>
2024-09-05 11:41:48 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const input = document.querySelector("input");
+const btn = document.getElementById("btn");
+const icon = document.querySelector(".icon");
+const weather = document.querySelector(".weather");
+const temperature = document.querySelector(".temperature");
+const description = document.querySelector(".description");
+const apiKey = "18061b1715ba4178dc6269181f71847e";
+
+btn.addEventListener("click", () => {
+  let city = input.value;
+  getWeather(city);
+});
+let getWeather = (city) => {
+  console.log(city);
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      const iconCode = data.weather[0].icon;
+      icon.innerHTML = `<img src = "https://openweathermap.org/img/wn/${iconCode}@2x.png" alt = "Weather icon"/>`;
+
+      const weatherCity = data.name;
+      const weatherCountry = data.sys.country;
+      weather.innerHTML = `${weatherCity},${weatherCountry}`;
+
+      let weatherTemp = data.main.temp;
+      weatherTemp = weatherTemp - 273;
+      const temp = weatherTemp.toFixed(2);
+      temperature.innerHTML = `${temp}°C`;
+
+      const weatherDes = data.weather[0].description;
+      description.innerHTML = `${weatherDes}`;
+    });
+};
